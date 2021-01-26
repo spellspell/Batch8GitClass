f685c73bb93282a4997bcb07c66985ce2eed8966
spellspell <spelspel12@gmail.com>
2021-01-26 13:35:45 -0500

    second commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello batch 8");
+
+        System.out.println("Second commit");
     }
 }
